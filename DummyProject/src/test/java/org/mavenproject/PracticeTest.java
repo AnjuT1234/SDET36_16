d3038fffb4c1f030dd8e4f12404983d3dc9c5b16
ANJU <ANJU@DESKTOP-4I82V8A>
2022-07-19 11:44:21 +0530

    committed

---
+++ DummyProject/src/test/java/org/mavenproject/PracticeTest.java
@@ -0,0 +1,39 @@
+package org.mavenproject;
+
+import org.testng.annotations.Test;
+
+public class PracticeTest {
+	
+	@Test
+	public void test1()
+	{
+		System.out.println("testing 1");
+		String browser=System.getProperty("BROWSER");
+		String url=System.getProperty("URL");
+		String username=System.getProperty("USERNAME");
+		String password=System.getProperty("PASSWORD");
+		
+		System.out.println("browser--->"+browser);
+		System.out.println("username--->"+username);
+		System.out.println("password--->"+password);
+		System.out.println("url--->"+url);
+	}
+	
+	@Test
+	public void test2()
+	{
+		System.out.println("testing 2");
+	}
+	
+	@Test
+	public void test3()
+	{
+		System.out.println("testing 3");
+	}
+	@Test
+	public void test4()
+	{
+		System.out.println("testing 4");
+	}
+
+}
---
+++ DummyProject/src/test/java/org/mavenproject/PracticeTest2.java
@@ -0,0 +1,30 @@
+package org.mavenproject;
+
+import org.testng.annotations.Test;
+
+public class PracticeTest2 {
+	
+	@Test
+	public void test5()
+	{
+		System.out.println("testing 5");
+	}
+	
+	@Test
+	public void test6()
+	{
+		System.out.println("testing 6");
+	}
+	
+	@Test
+	public void test7()
+	{
+		System.out.println("testing 7");
+	}
+	@Test
+	public void test8()
+	{
+		System.out.println("testing 8");
+	}
+
+}
